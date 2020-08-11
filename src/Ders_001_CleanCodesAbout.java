ab5f5bc22e29bebc88b444eb837fb68e42c404b2
Hamit Mizrak <hamitmizrak@gmail.com>
2020-08-11 22:06:33 +0300

    Clean codes hakkında söyleyişi

---
+++ src/Ders_001_CleanCodesAbout.java
@@ -0,0 +1,29 @@
+
+//Clean codes
+/*
+ 
+ clean codes:Az kod çok iş mantığı (Hamit.Mızrak)
+ Aslında Temiz kod yazmak için programala yaptığımız dilin özelliklerini çok iyi bilmemiz gerekiyor. Aşağıda sorduğum bir kaç özellikleri bilmeniz gerekiyor.
+ 
+ kodlarımızın daha dinamik, sadece  eblli başlı yapıları değiştirirek düzenleyebiliyoruz. Yoksa codeslerımız bad smell 
+ throw new Exception("Deneme");
+ SingleTon Design pattern hakıında ne biliyoruz?
+ overloading ,overriding nedir?
+ Metotları kullanmak?
+ primitive kullanmak veya wrapper classları kullanmak?
+ interface, extends kullanmak?
+ MVC kullanmak?
+ Debug bize yararı ?
+ Compiler,Interpreter nedir?
+ Collection kullanmak?
+ Heap memory , Stack memory?
+ JVM(JIT,Interpreter)  <  JRE (Api+Libraries)  <  JDK (Compiler+Debug) ?
+ boxing 
+ POJO,Bean,CDI kullanmak
+ Dinamik diziler ,Statik diziler
+ Bağlantılarda: Statement yerine PreparedStatement kullanmak
+
+ */
+public class Ders_001_CleanCodesAbout {
+
+}
